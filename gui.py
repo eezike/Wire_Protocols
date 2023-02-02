6de4670b378843a304d5bd4a3ea1db27a1f4d8c9
Emeka Ezike <franklinezike@gmail.com>
2023-02-02 11:16:33 -0500

    homepage done

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class LoginFrame(tk.Frame):
+class LoginPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
 
@@ -9,43 +9,51 @@ class LoginFrame(tk.Frame):
 
         # Create label for host
         self.host_label = tk.Label(self, text="Host:", font=("TkDefaultFont", 16))
-        self.host_label.pack()
+        self.host_label.grid(row=0, column=0, padx=10, pady=10)
         
         # Create entry for host
         self.host_entry = tk.Entry(self, font=("TkDefaultFont", 14))
-        self.host_entry.pack()
+        self.host_entry.grid(row=0, column=1, padx=10, pady=10)
 
         # Create label for port
         self.port_label = tk.Label(self, text="Port:", font=("TkDefaultFont", 16))
-        self.port_label.pack()
+        self.port_label.grid(row=1, column=0, padx=10, pady=10)
         
         # Create entry for port
         self.port_entry = tk.Entry(self, font=("TkDefaultFont", 14))
-        self.port_entry.pack()
+        self.port_entry.grid(row=1, column=1, padx=10, pady=10)
         
         # Create label for username
         self.username_label = tk.Label(self, text="Username:", font=("TkDefaultFont", 16))
-        self.username_label.pack()
+        self.username_label.grid(row=2, column=0, padx=10, pady=10)
         
         # Create entry for username
         self.username_entry = tk.Entry(self, font=("TkDefaultFont", 14))
-        self.username_entry.pack()
+        self.username_entry.grid(row=2, column=1, padx=10, pady=10)
         
         # Create label for password
         self.password_label = tk.Label(self, text="Password:", font=("TkDefaultFont", 16))
-        self.password_label.pack()
+        self.password_label.grid(row=3, column=0, padx=10, pady=10)
         
         # Create entry for password
         self.password_entry = tk.Entry(self, font=("TkDefaultFont", 14), show="*")
-        self.password_entry.pack()
+        self.password_entry.grid(row=3, column=1, padx=10, pady=10)
         
         # Create login button
         self.login_button = tk.Button(self, text="Login", font=("TkDefaultFont", 14), command=self.login)
-        self.login_button.pack()
+        self.login_button.grid(row=4, column=0, pady=10, padx=10, sticky="W")
+
+        # Create register button
+        self.register_button = tk.Button(self, text="Register", font=("TkDefaultFont", 14), command=self.register)
+        self.register_button.grid(row=4, column=1, pady=10, padx=10, sticky="E")
     
-    def login(self):
+    def connect(self):
         host = self.host_entry.get()
         port = self.port_entry.get()
+
+    
+    def login(self):
+        self.connect()
         username = self.username_entry.get()
         password = self.password_entry.get()
         
@@ -54,6 +62,14 @@ class LoginFrame(tk.Frame):
             self.master.switch_frame(HomePage)
         else:
             messagebox.showerror("Login Failed", "Invalid username or password")
+    
+    def register(self):
+        self.connect()
+        username = self.username_entry.get()
+        password = self.password_entry.get()   
+        
+        self.master.switch_frame(HomePage)
+
 
 
 class HomePage(tk.Frame):
@@ -61,17 +77,41 @@ class HomePage(tk.Frame):
         tk.Frame.__init__(self, master)
 
         self.master = master
+
+        self.messages_list = tk.Listbox(self, height=15, width=50)
+        self.messages_list.grid(row=0, column=0, rowspan=2, padx=10, pady=10)
+
+        self.recipient = tk.StringVar()
+
+        options = ["user1", "user2", "user3"]
+        dropdown = tk.OptionMenu(self, self.recipient, *options, command=self.recipient_selected)
+        dropdown.grid(row=0, column=1, padx=10, pady=10)
+
+        self.message_input = tk.Text(self, height=3, width=30)
+        self.message_input.grid(row=0, column=2, sticky="W", padx=10, pady=10)
+
+
+        send_button = tk.Button(self, text="Send", command=self.send_message)
+        send_button.grid(row=1, column=1, sticky="E", padx=10, pady=10)
         
-        tk.Label(self, text="Home Page").pack()
-        tk.Button(self, text="Go Back", command=lambda: master.switch_frame(LoginFrame)).pack()
+    
+    def send_message(self):
+        message = self.message_input.get("1.0", 'end-1c')
+        self.messages_list.insert("end", "You: " + message)
+        self.message_input.delete("1.0", tk.END)
+
+    def recipient_selected(self, *args):
+        selected_option = self.recipient.get()
+        print(f"Recepient: {selected_option}")
 
 
 class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self._frame = None
-        self.geometry("400x300")
-        self.switch_frame(LoginFrame)
+        self.title("Chatroom")
+        self.geometry("850x300")
+        self.switch_frame(HomePage)
 
     def switch_frame(self, frame_class):
         new_frame = frame_class(self)
